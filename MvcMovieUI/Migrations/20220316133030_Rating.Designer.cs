564506c914631d60f4dff6c4c52c3eb27e7a4df0
Cedd on Laptop <cedricvincenthije@gmail.com>
2022-03-16 21:31:14 +0800

    Added new field Rating

--- MvcMovieUI/Migrations/20220316133030_Rating.Designer.cs
+++ MvcMovieUI/Migrations/20220316133030_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovieUI.Data;
 
@@ -11,9 +12,10 @@ using MvcMovieUI.Data;
 namespace MvcMovieUI.Migrations
 {
     [DbContext(typeof(MvcMovieUIContext))]
-    partial class MvcMovieUIContextModelSnapshot : ModelSnapshot
+    [Migration("20220316133030_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
